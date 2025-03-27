ae1ae9d0ec7f48995f512e4072927b35d99ff918
吴鑫淼 <wuxinmiao@MiaoMiao-MacBook.local>
2025-03-27 15:12:41 +0800

    add PlasmidTransModel module

---
+++ PlasmidTransModel/PlasmidTransModel.R
@@ -0,0 +1,59 @@
+library(tidymodels)
+library(tidyverse)
+library(Biostrings)
+library(seqinr)
+inputgenome="GCA_015356015__CP064244.1.fasta"
+outputnam="modelmerge"
+#modeltype=c("Binary","ThreeClass")
+modeltype="ThreeClass"
+if(modeltype=="Binary"){
+	load("binaryModel.Rdata")
+	user_file=readDNAStringSet(filepath =inputgenome,format="fasta")
+	pentamer_freq=as.data.frame(oligonucleotideFrequency(user_file, width=5,as.prob= TRUE, with.labels = T))
+	pentamer_freq=pentamer_freq%>%mutate(plasmids=names(user_file))%>%select(plasmids,everything())
+	pentamer_trans=unlist(pentamer_freq[1,2:dim(pentamer_freq)[2]])
+	forinput_kmer=rep(0,each=length(feature_best))
+	forinput_kmer=sapply(1:length(forinput_kmer),function(xxx){if(feature_best[xxx]%in%names(pentamer_trans)){pentamer_trans[names(pentamer_trans)%in%feature_best[xxx]]}else{0}},USE.NAMES=FALSE)
+	names(forinput_kmer)=feature_best
+	newfit=workflow_best%>%fit(fulldata_extract)
+	modelinput=matrix(forinput_kmer,nrow=1)
+	colnames(modelinput)=feature_best
+	pre=augment(newfit,modelinput)
+	write_tsv(pre,sprintf("%s_prediction_2class.txt",outputnam))
+}else{
+	load("threeClassModel.Rdata")
+	genefeature=c("group_13847","group_1599","group_5662","group_13427")
+	kmerfeature=c("CAGGC" , "ACAGA" , "GACGC" , "GGCCG" ,"AGAGA" ,"CTCCG" ,"CTCTT",
+	"ACATC" ,"CTGGT", "GCGGC"   ,"TTGTT"  , "CCGGT", "TCAGC" ,"GAAGG")
+	prodigalcode=sprintf("prodigal -a target.pep -d target.cds -g 11 -p single -i %s",inputgenome)
+	makedb="makeblastdb -in model3.fasta  -dbtype prot -title model -out model"
+	blastpcode="blastp -db model -query target.pep -out model.out -evalue 0.00001 -outfmt 6 -max_target_seqs 10000000 -max_hsps 1000 -qcov_hsp_perc 80 -num_threads 4"
+	system(prodigalcode)
+	system(makedb)
+	system(blastpcode)
+	#gene feature
+	readout=read_tsv("model.out",col_names=FALSE)
+	if(dim(readout)[1]==0){
+		 uniquematch=c()
+	}else{
+		readout01=readout%>%filter(X3>95)%>%group_by(X1)%>%filter(X3==max(X3))
+		readout02=as.data.frame(readout01)
+		uniquematch=unique(readout02$X2)
+	}
+	forinput_gene=rep(0,each=4)
+	names(forinput_gene)=genefeature
+	forinput_gene[forinput_gene%in%uniquematch]=1
+	#kmer feature
+	user_file=readDNAStringSet(filepath =inputgenome,format="fasta")
+	pentamer_freq=as.data.frame(oligonucleotideFrequency(user_file, width=5,as.prob= TRUE, with.labels = T))
+	pentamer_freq=pentamer_freq%>%mutate(plasmids=names(user_file))%>%select(plasmids,everything())
+	pentamer_trans=unlist(pentamer_freq[1,2:dim(pentamer_freq)[2]])
+	forinput_kmer=rep(0,each=14)
+	forinput_kmer=sapply(1:length(forinput_kmer),function(xxx){if(kmerfeature[xxx]%in%names(pentamer_trans)){pentamer_trans[names(pentamer_trans)%in%kmerfeature[xxx]]}else{0}},USE.NAMES=FALSE)
+	names(forinput_kmer)=kmerfeature
+	getfeatureinput=c(forinput_gene,forinput_kmer)
+	newfit=workflow_best%>%fit(fulldata_extract)
+	pre=augment(newfit,t(as.data.frame(getfeatureinput)))
+	write_tsv(pre,sprintf("%s_prediction_3class.txt",outputnam))
+#
+}
---
+++ PlasmidTransModel/PlasmidTransModel.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+#echo original parameters=[$@]
+
+#-o或--options选项后面是可接受的短选项，如ab:c::，表示可接受的短选项为-a -b -c，
+#其中-a选项不接参数，-b选项后必须接参数，-c选项的参数为可选的
+#-l或--long选项后面是可接受的长选项，用逗号分开，冒号的意义同短选项。
+#-n选项 后接选项解析错误时提示的脚本名字
+ARGS=`getopt -o ha:o:m: --long inputGenome:,output_tag:,modelType: -n "$0" -- "$@"`
+if [ $? != 0 ]; then
+    echo "Terminating..."
+    exit 1
+fi
+
+#echo ARGS=[$ARGS]
+#将规范化后的命令行参数分配至位置参数（$1,$2,...)
+eval set -- "${ARGS}"
+#echo formatted parameters=[$@]
+
+while true
+do
+    case "$1" in
+	-h|--help)
+      echo "Usage: PlasmidTransModel.sh -a inputGenome -o output_tag -m modelType"
+	    echo "-a|--inputGenome the input file in fasta format"
+	    echo "-o|--output_tag the prefix of the output file"
+	    echo "-m|--modelType the model type to be choosen, 
+	    		  two types of models can be choosen: Binary or ThreeClass"
+	    exit 1
+	    ;;
+        -a|--inputGenome)
+            #echo "Option a";
+	    inputGenome=$2
+            shift 2
+            ;;
+        -o|--output_tag)
+            #echo "Option b, argument $2";
+	    output_tag=$2
+            shift 2
+            ;;
+        -m|--modelType)
+            #echo "Option a";
+	    modelType=$2
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Internal error!"
+            exit 1
+            ;;
+    esac
+done
+#
+echo $inputGenome,$output_tag,$modelType
+#
+Rscript PlasmidTransModel.R $inputGenome $output_tag $modelType
